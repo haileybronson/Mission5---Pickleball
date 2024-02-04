a5e823dd13d741db261a6e38573f8d322a7eece1
haileybronson <haileybronson@HAILEYBRONSF5EF>
2024-02-03 21:19:19 -0700

    Add project files.

---
+++ Mission5/wwwroot/js/calculator.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $('#calculate').click(function () {
+        // Validation to make sure they 
+        //actually entered a positive number
+        var hours = $('#hours').val();
+        if (hours === '' || isNaN(hours) || hours <= 0) {
+            alert('Please enter a positive number for hours.');
+            return false;
+        }
+
+        // calculation
+        var rate = $('#rate').val();
+        var total = parseFloat(hours) * parseFloat(rate);
+
+        // result
+        $('#total').val(total.toFixed(2)); // 2 dec places
+    });
+});
