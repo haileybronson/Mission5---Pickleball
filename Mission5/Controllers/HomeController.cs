a5ce09c3bcdfd8c0bf91f307c4fa37a742081868
haileybronson <haileyp7@byu.edu>
2024-02-05 14:58:34 -0700

    renamed file, referenced the layout file on each view instead

--- Mission5/Controllers/HomeController.cs
+++ Mission5/Controllers/HomeController.cs
@@ -6,27 +6,16 @@ namespace Mission5.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Hailey()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
